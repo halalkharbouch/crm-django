5b2cd84bc7d731386c8dfc86e3f164d34cf35713
Halal Kharbouch <binyaminbgod@gmail.com>
2024-08-13 15:28:08 +0100

    add lead, lead_detail, lead_create views

--- leads/models.py
+++ leads/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
 
 class Agent(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.user.email
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
@@ -10,4 +17,6 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
 
